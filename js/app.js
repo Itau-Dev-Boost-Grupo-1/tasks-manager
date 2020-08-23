f99089e763b460fb18f542d877ffffdcc9fe0691
Vinicius Ribeiro <hello@vncsrbro.com>
2020-08-23 19:53:41 -0300

    :sparkles: Added individual controls to tasks and total counter

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ let tasksList = [];
  * Add new Task
  */
 
-function addNewTask(value) {
+function addNewTask(text) {
   const newTask = {
-    value,
+    text,
     id: Date.now(),
+    seconds: 0,
   };
 
   tasksList.push(newTask);
@@ -20,7 +21,7 @@ function addNewTask(value) {
 
 const form = document.querySelector(".add-task__form");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const formInput = document.querySelector(".add-task__form-input");
@@ -45,20 +46,20 @@ function renderTasks(task) {
   taskItem.setAttribute("data-key", `${task.id}`);
 
   taskItem.innerHTML = `
-    <p class="tasks__list-item-title">${task.value}</p>
+    <p class="tasks__list-item-title">${task.text}</p>
 
-    <ul class="tasks__list-item-timer">
-      <li><span class="tasks__list-item-hours">00</span>:</li>
-      <li><span class="tasks__list-item-minutes">00</span>:</li>
-      <li><span class="tasks__list-item-seconds">00</span></li>
+    <ul class="timer">
+      <li><span class="timer__hours">00</span>:</li>
+      <li><span class="timer__minutes">00</span>:</li>
+      <li><span class="timer__seconds">00</span></li>
     </ul>
 
     <div class="tasks__list-item-controls">
-      <button class="tasks__list-item-button tasks__list-item-pause">
+      <button class="tasks__list-item-button tasks__list-item-pause"  onclick="pause(${task.id})">
         <img src="./img/icon__pause.svg" alt="Pause icon">
       </button>
 
-      <button class="tasks__list-item-button tasks__list-item-play" onClick="startTimer(${task.id})">
+      <button class="tasks__list-item-button tasks__list-item-play" onclick="start(${task.id})">
         <img src="./img/icon__play.svg" alt="Play icon">
       </button>
     </div>
@@ -68,26 +69,113 @@ function renderTasks(task) {
 }
 
 /**
- * Start Timer
+ * Format Time
  */
 
 function formatTime(value) {
   return value > 9 ? value : "0" + value;
 }
 
-function startTimer(id) {
-  let seconds = 0;
+/**
+ * Start Timer
+ */
 
-  const counter = setInterval(function () {
+function start(id) {
+  const currentItem = tasksList.find(function (item) {
+    return item.id === id;
+  });
+
+  const currentPlayButton = document.querySelector(
+    `[data-key='${id}'] .tasks__list-item-play`
+  );
+
+  currentPlayButton.setAttribute("disabled", "true");
+
+  currentItem.interval = setInterval(function () {
+    ++currentItem.seconds;
+
+    const hoursField = document.querySelector(
+      `[data-key='${id}'] .timer__hours`
+    );
+
     const minutesField = document.querySelector(
-      `[data-key='${id}'] .tasks__list-item-minutes`
+      `[data-key='${id}'] .timer__minutes`
     );
 
     const secondsField = document.querySelector(
-      `[data-key='${id}'] .tasks__list-item-seconds`
+      `[data-key='${id}'] .timer__seconds`
     );
 
-    minutesField.innerHTML = formatTime(parseInt(seconds / 60, 10));
-    secondsField.innerHTML = formatTime(++seconds % 60);
+    hoursField.innerHTML = formatTime(parseInt(currentItem.seconds / 3600));
+    minutesField.innerHTML = formatTime(
+      parseInt((currentItem.seconds % 3600) / 60, 10)
+    );
+    secondsField.innerHTML = formatTime(currentItem.seconds % 60);
   }, 1000);
+}
+
+/**
+ * Pause Timer
+ */
+
+function pause(id) {
+  const currentItem = tasksList.find(function (item) {
+    return item.id === id;
+  });
+
+  const currentPlayButton = document.querySelector(
+    `[data-key='${id}'] .tasks__list-item-play`
+  );
+
+  currentPlayButton.removeAttribute("disabled");
+
+  clearInterval(currentItem.interval);
+}
+
+/**
+ * Stop all timers
+ */
+
+function stopAllTimers() {
+  tasksList.map(function (item) {
+    clearInterval(item.interval);
+  });
+
+  const allPlayButtons = document.querySelectorAll(".tasks__list-item-play");
+
+  allPlayButtons.forEach((button) => {
+    button.removeAttribute("disabled");
+  });
+}
+
+/**
+ * Total Counter
+ */
+
+function totalCounter() {
+  stopAllTimers();
+
+  var sumOfSeconds = tasksList.reduce(function (prevVal, elem) {
+    return prevVal + elem.seconds;
+  }, 0);
+
+  const totalHours = parseInt(sumOfSeconds / 3600);
+  const totalMinutes = parseInt((sumOfSeconds % 3600) / 60, 10);
+  const totalSeconds = sumOfSeconds % 60;
+
+  const hoursField = document.querySelector(
+    ".tasks__total-timer .timer__hours"
+  );
+
+  const minutesField = document.querySelector(
+    ".tasks__total-timer .timer__minutes"
+  );
+
+  const secondsField = document.querySelector(
+    ".tasks__total-timer .timer__seconds"
+  );
+
+  hoursField.innerHTML = formatTime(totalHours);
+  minutesField.innerHTML = formatTime(totalMinutes);
+  secondsField.innerHTML = formatTime(totalSeconds);
 }
